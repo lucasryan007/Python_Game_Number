a463f6e2f3725c22b801a0324a09ac780f66900f
Lucas Ruan <lucasruan901@gmail.com>
2024-02-14 01:31:24 -0300

    Implementação de laço e simplificação de código

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,18 +1,32 @@
-#Mensagem de Boas Vindas :)
-print("* Bem vindo ao jogo de Adivinhação *")
+#Mensagem de Boas Vindas
+print(" Bem vindo ao jogo de Adivinhação :) Você terá 3 chances para advinhar o número de 1 até 50 *")
+
+#Tentativas até o Game Over
+total_de_tentativas = 3
+
+#Condição para jogo
+while(total_de_tentativas > 0):
+    print("Quantidade de Chances", total_de_tentativas)
 
 #Variáveis
-numero_secreto = 42
-chute_str= input("Digite o seu número: ")
-chute = int(chute_str)
-acertou_numero = numero_secreto == chute
-errou_numero = numero_secreto < chute
+    numero_secreto = 42
+    chute_str = input("Digite o seu número: ")
+    chute = int(chute_str)
+    acertou_numero = numero_secreto == chute
+    errou_numero = numero_secreto < chute
 
 #Funções
-if(acertou_numero):
-    print("Você acertou!!!!!!")
-elif (errou_numero):
+    if (acertou_numero):
+        total_de_tentativas -=3
+    elif (errou_numero):
         print("Você errouuu! O seu chute foi maior do que o número secreto")
-else:
+        total_de_tentativas -= 1
+    else:
         print("Você errouuu! O seu chute foi menor do que o número secreto")
-print("Fim de jogo")
+        total_de_tentativas -= 1
+
+#Mensagem final do jogo
+if (acertou_numero) == True:
+    print("Você acertou :) Fim de Jogo")
+elif total_de_tentativas == 0:
+    print("Fim de jogo, você não descobriu o número :(")
