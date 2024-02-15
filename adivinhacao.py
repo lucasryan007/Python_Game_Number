868e57fb58bb536437aec0e04925a992105436f5
Lucas Ruan <lucasruan901@gmail.com>
2024-02-15 15:03:24 -0300

    mudança de laço, while para for e otimizacao de linhas

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,32 +1,29 @@
-#Mensagem de Boas Vindas
-print(" Bem vindo ao jogo de Adivinhação :) Você terá 3 chances para advinhar o número de 1 até 50 *")
+# Mensagem de Boas Vindas
+print("*Bem vindo ao jogo de Adivinhação :) Você terá 3 chances para advinhar o número de 1 até 50*")
 
-#Tentativas até o Game Over e rodadas
+# Tentativas até o Game Over e rodadas
 total_de_tentativas = 3
-rodada = 1
 
-#Condição para jogo
-while(rodada <= total_de_tentativas):
+# Definição de número secreto
+numero_secreto = 42
+
+# Condição para jogo
+for rodada in range (1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
-#Variáveis
-    numero_secreto = 42
-    chute_str = input("Digite o seu número: ")
-    chute = int(chute_str)
+    chute = int(input("Digite o seu número: "))
     acertou_numero = numero_secreto == chute
-    errou_numero = numero_secreto < chute
+    chute_maior = numero_secreto < chute
+    chute_menor = numero_secreto > chute
 
-#Funções
-    if (errou_numero):
-        rodada = rodada + 1
+    # Funções acerto ou erro
+    if (acertou_numero):
+        print("Você acertou :) Fim de jogo!")
+        break
+    elif (chute_maior):
         print("Você errou! O seu chute foi maior do que o número secreto!")
-    else:
-        rodada = rodada + 1
+    elif (chute_menor):
         print("Você errou! O seu chute foi menor do que o número secreto!")
 
-
-#Mensagem final do jogo
-if (acertou_numero) == True:
-    print("Você acertou :) Fim de Jogo")
-elif total_de_tentativas == 0:
-    print("Fim de jogo, você não descobriu o número :(")
+if not (acertou_numero):
+    print("Fim de jogo, você não descobriu o número secreto que era: {}".format(numero_secreto))
