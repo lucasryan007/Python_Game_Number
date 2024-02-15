ed2fa41d5d82148e2b7984527206bc852b7be457
Lucas Ruan <lucasruan901@gmail.com>
2024-02-14 21:04:10 -0300

    adicao de rodadas ao jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,12 +1,13 @@
 #Mensagem de Boas Vindas
 print(" Bem vindo ao jogo de Adivinhação :) Você terá 3 chances para advinhar o número de 1 até 50 *")
 
-#Tentativas até o Game Over
+#Tentativas até o Game Over e rodadas
 total_de_tentativas = 3
+rodada = 1
 
 #Condição para jogo
-while(total_de_tentativas > 0):
-    print("Quantidade de Chances", total_de_tentativas)
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
 #Variáveis
     numero_secreto = 42
@@ -16,14 +17,13 @@ while(total_de_tentativas > 0):
     errou_numero = numero_secreto < chute
 
 #Funções
-    if (acertou_numero):
-        total_de_tentativas -=3
-    elif (errou_numero):
-        print("Você errouuu! O seu chute foi maior do que o número secreto")
-        total_de_tentativas -= 1
+    if (errou_numero):
+        rodada = rodada + 1
+        print("Você errou! O seu chute foi maior do que o número secreto!")
     else:
-        print("Você errouuu! O seu chute foi menor do que o número secreto")
-        total_de_tentativas -= 1
+        rodada = rodada + 1
+        print("Você errou! O seu chute foi menor do que o número secreto!")
+
 
 #Mensagem final do jogo
 if (acertou_numero) == True:
