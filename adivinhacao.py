246b878e2f0d43e48a569279c0018fd3031e9d57
Lucas Ruan <lucasruan901@gmail.com>
2024-02-16 15:44:14 -0300

    adicao de limitador entre numero minimo e maximo

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,23 +1,28 @@
-# Mensagem de Boas Vindas
-print("*Bem vindo ao jogo de Adivinhação :) Você terá 3 chances para advinhar o número de 1 até 50*")
-
-# Tentativas até o Game Over e rodadas
-total_de_tentativas = 3
-
-# Definição de número secreto
+# Dificuldade do jogo
+total_de_tentativas = 5
 numero_secreto = 42
+numero_minimo = 1
+numero_maximo = 100
+
+# Mensagem de Boas Vindas
+print("*Bem vindo ao jogo de Adivinhação :) Você terá {} chances para advinhar o número secreto*".format(total_de_tentativas))
 
 # Condição para jogo
 for rodada in range (1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite um número entre {} e {}: ".format(numero_minimo, numero_maximo)))
     acertou_numero = numero_secreto == chute
     chute_maior = numero_secreto < chute
     chute_menor = numero_secreto > chute
 
     # Funções acerto ou erro
-    if (acertou_numero):
+    if (chute < 1):
+        print("Você digitou um número menor do que {}".format(numero_minimo))
+        continue
+    elif (chute > 100):
+        print("Você digitou um número maior do que {}".format(numero_maximo))
+        continue
+    elif (acertou_numero):
         print("Você acertou :) Fim de jogo!")
         break
     elif (chute_maior):
